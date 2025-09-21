901d473e78866f4941632595c6d3f77adfd1b3c3
namnguyen31241024675-ctrl <namnguyen.31241024675@st.ueh.edu.vn>
2025-09-22 03:57:26 +0700

    t cung submit nha m

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// Thành viên 3 - feature-member3
+
+using System;
+
+public class Calculator
+{
+    // Hàm tính tổng của hai số
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Calculator calculator = new Calculator();
+        int result = calculator.Add(5, 3); // Tính tổng của 5 và 3
+        Console.WriteLine("Tổng của 5 và 3 là: " + result); // Hiển thị kết quả
+    }
+}
