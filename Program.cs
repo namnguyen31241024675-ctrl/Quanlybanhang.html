dd305be71c94d12419493ac1da1953d8c9353183
namnguyen31241024675-ctrl <namnguyen.31241024675@st.ueh.edu.vn>
2025-09-22 03:16:51 +0700

    submit bai lam

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// Thành viên 1 - feature-member1
+
+using System;
+
+public class Calculator
+{
+    // Hàm cộng 2 số
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Calculator calc = new Calculator();
+        Console.WriteLine("Tổng của 2 và 3 là: " + calc.Add(2, 3));
+    }
+}
